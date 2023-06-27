3a001ec17ffc5aee41db387604f78c50adf957c2
Rohitha-M-Raghu <rohitha13raghu00@gmail.com>
2023-06-27 22:07:38 +0530

    projects

---
+++ projects/to-do-list/to-do-list.js
@@ -0,0 +1,56 @@
+// retrieve from local storage
+// const todoList = [];
+let todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+
+renderTodoList();
+
+document.querySelector('.js-add-todo-button')
+  .addEventListener('click', () => {
+    addTodo();
+  });
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value || 'No DueDate';
+  
+  todoList.push({name, dueDate});
+
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+
+  // to clear the input box
+  inputElement.value = ''; 
+
+  renderTodoList();
+}
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach((todoObject, index) => {
+    const {name, dueDate} = todoObject;
+
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class ="delete-todo-button js-delete-todo-button">Delete</button>
+      `;
+
+    todoListHTML += html;
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+
+  });
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1); 
+        renderTodoList();
+      });
+    });
+
+ }
---
+++ projects/ultimate-rock-paper-scissors/rock-paper-scissors.js
@@ -0,0 +1,162 @@
+let computerMove;
+
+let score = JSON.parse(localStorage.getItem("score")) || {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+};
+
+updateScoreElement();
+
+function robotMoveGenerator() {
+  const randomNumber = Math.random();
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    return "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function displayResult(user, result) {
+  // console.log("My move: " + user);
+  // console.log("Robot move: " + computerMove);
+  // console.log(result);
+  updateScore(result);
+  let displayMessage = `You picked ${user}. Computer picked ${computerMove}.`;
+
+  document.querySelector(".js-result").innerHTML = result;
+
+  document.querySelector(".js-moves").innerHTML = `You
+<img src="images/${user}-emoji.png" class="move-icon" alt="None" />
+<img src="images/${computerMove}-emoji.png" class="move-icon" alt="None" />
+Computer`;
+}
+
+function updateScoreElement() {
+  document.querySelector(
+    ".js-score"
+  ).innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
+}
+function updateScore(result) {
+  if (result === "You win!") {
+    score.wins++;
+  } else if (result === "You lose!") {
+    score.losses++;
+  } else {
+    score.ties++;
+  }
+  // saving to local storage
+  // it only supports strings
+  localStorage.setItem("score", JSON.stringify(score));
+
+  updateScoreElement();
+}
+
+document.querySelector('.js-reset-score-button')
+  .addEventListener('click', () => {
+    resetScore();
+});
+
+
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem("score");
+  updateScoreElement();
+}
+
+document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+    play('rock');
+});
+
+document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+    play('paper');
+});
+
+
+document.querySelector('.js-scissors-button')
+  .addEventListener('click', () => {
+    play('scissors');
+});
+
+
+function play(userMove) {
+  computerMove = robotMoveGenerator();
+  let result = "";
+  if (computerMove === userMove) {
+    result = "Tie!";
+    displayResult(userMove, result);
+  } else if (userMove === "rock") {
+    if (computerMove === "paper") {
+      result = "You lose!";
+      displayResult(userMove, result);
+    } else {
+      result = "You win!";
+      displayResult(userMove, result);
+    }
+  } else if (userMove === "paper") {
+    if (computerMove === "rock") {
+      result = "You win!";
+      displayResult(userMove, result);
+    } else {
+      result = "You lose!";
+      displayResult(userMove, result);
+    }
+  } else {
+    if (computerMove === "paper") {
+      result = "You win!";
+      displayResult(userMove, result);
+    } else {
+      result = "You lose!";
+      displayResult(userMove, result);
+    }
+  }
+}
+
+document.querySelector('.js-auto-play-button')
+  .addEventListener('click', () => {
+    autoPlay();
+});
+
+
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+  if(!isAutoPlaying) {
+    intervalId = setInterval(() => {
+      const playerMove = robotMoveGenerator();
+      play(playerMove);
+    }, 1000);
+    isAutoPlaying = true;
+    document.querySelector('.auto-play-button').innerHTML = 'Stop Auto Play';
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+    document.querySelector('.auto-play-button').innerHTML = 'Auto Play';
+  }
+  
+}
+
+// to play using keyboard
+document.body.addEventListener('keydown', (event) => {
+  if(event.key === 'r') {
+    play('rock');
+  } else if(event.key === 'p') {
+    play('paper');
+  } else if (event.key === 's') {
+    play('scissors');
+  } else if (event.key === 'z') {
+    resetScore();
+  } else if (event.key === 'a') {
+    autoPlay();
+  }
+
+});
+
+
